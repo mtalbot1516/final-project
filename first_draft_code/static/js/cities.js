0e8d3a852e9a6f28e1e92f94f07b9e97d96edd48
JimmyJ-D <75762210+JimmyJ-D@users.noreply.github.com>
2021-05-13 18:47:56 -0500

    demo beta code

---
+++ first_draft_code/static/js/cities.js
@@ -0,0 +1,37 @@
+// double check varible I changed it from cities to houses and modified the json
+let cities = [{
+    location: [36.055, -86.678],
+    city: "Antioch",
+    state: "TN",
+    street: "Shihmen",
+    price : 295000
+},
+{
+    location: [36.029, -86.697],
+    city: "Antioch",
+    state: "TN",
+    street: "Somerset",
+    price : 395500
+},
+{
+    location: [36.075, -86.705],
+    city: "Nashville",
+    state: "TN",
+    street: "Bell_RD",
+    price : 275000
+},
+{
+    location: [36.031, -86.697],
+    city: "Antioch",
+    state: "TN",
+    street: "Somerset",
+    price : 390000
+},
+{
+    location: [36.019, -86.725],
+    city: "Nashville",
+    state: "TN",
+    street: "Holt",
+    price : 479000
+}
+];
