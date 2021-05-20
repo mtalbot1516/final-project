dc84c5f7b3cace4f4ca3ada87650b8e5270f014e
JimmyJ-D <75762210+JimmyJ-D@users.noreply.github.com>
2021-05-19 19:14:28 -0500

    front end dashboard

---
+++ MTJB_Real_Estate/static/js/cities.js
@@ -0,0 +1,42 @@
+// double check varible I changed it from cities to houses and modified the json
+let cities = [{
+    location: [36.055, -86.678],
+    city: "Antioch",
+    state: "TN",
+    street: "Shihmen",
+    price : 295000,
+    BB: "3BD_3BA"
+},
+{
+    location: [36.029, -86.697],
+    city: "Antioch",
+    state: "TN",
+    street: "Somerset",
+    price : 395500,
+    BB: "3BD_3BA"
+},
+{
+    location: [36.075, -86.705],
+    city: "Nashville",
+    state: "TN",
+    street: "Bell_RD",
+    price : 275000,
+    BB: "3BD_BA"
+},  
+{
+    location: [36.031, -86.697],
+    city: "Antioch",
+    state: "TN",
+    street: "Somerset",
+    price : 390000,
+    BB:"3BD_3BA"
+},
+{
+    location: [36.019, -86.725],
+    city: "Nashville",
+    state: "TN",
+    street: "Holt",
+    price : 479000,
+    BB:"3BD_3BA"
+}
+];
